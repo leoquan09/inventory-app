c339f7b24e8f7c0d9269d7e527d88ee0c0c529b2
Leo <leoquan09@gmail.com>
2026-06-02 17:38:47 -0700

    added connection string

--- db/pool.js
+++ db/pool.js
@@ -1,5 +1,5 @@
 const { Pool } = require('pg');
 
 module.exports = new Pool({
-    connectionString: 'postgresql://postgres:09272011@localhost:5432/inventory'
+    connectionString: 'process.env.DATABASE_URL'
 });
